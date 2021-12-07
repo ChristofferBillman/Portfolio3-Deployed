28e8507651127e6cdea4b62d555041e8698afe1d
ChristofferBillman <christoffer.billman@gmail.com>
2021-12-07 13:31:47 +0100

    Fixed broken paths to img and js.

--- posts.js
+++ posts.js
@@ -2,9 +2,9 @@ let posts = [{
 	"title": "Portfolio v.3",
 	"body": "Den tredje försöket till en portfoliosida! Denna version kom faktiskt till, och projektet blev mycket mer ambitiöst än vad de tidigare två var! Två flugor en smäll!",
 	"images": [
-		"/img/portfolio3/grafisk-profil-rosa.webp",
-		"/img/portfolio3/grafisk-profil.webp",
-		"/img/portfolio3/preview.webp"
+		"img/portfolio3/grafisk-profil-rosa.webp",
+		"img/portfolio3/grafisk-profil.webp",
+		"img/portfolio3/preview.webp"
 	],
 	"imagePosition": "right",
 	"order": "1"
@@ -12,14 +12,14 @@ let posts = [{
 	"title": "Skissteknik - Rumbokningsystem",
 	"body": "Denna uppgift var del av kursen 'Design för användarupplevelse' hos designhögskolan i Umeå. Det gick huvudsakligen ut på att lära sig hur man arbetar med wireframes och iterativa designprocesser",
 	"images": [
-		"/img/skissteknik/m1.webp",
-		"/img/skissteknik/m2.webp",
-		"/img/skissteknik/m3.webp",
-		"/img/skissteknik/m4.webp",
-		"/img/skissteknik/m5.webp",
-		"/img/skissteknik/m6.webp",
-		"/img/skissteknik/m7.webp",
-		"/img/skissteknik/m8.webp"
+		"img/skissteknik/m1.webp",
+		"img/skissteknik/m2.webp",
+		"img/skissteknik/m3.webp",
+		"img/skissteknik/m4.webp",
+		"img/skissteknik/m5.webp",
+		"img/skissteknik/m6.webp",
+		"img/skissteknik/m7.webp",
+		"img/skissteknik/m8.webp"
 	],
 	"imagePosition": "top",
 	"order": "2"
@@ -27,10 +27,10 @@ let posts = [{
 	"title": "Portfolio v.2",
 	"body": "Andra försöket till en portfoliosida - gick sådär. Blev aldrig riktigt nöjd med designen så det projektet kom som längst med Hi-Fi prototyper i Adobe XD.",
 	"images": [
-		"/img/portfolio2/cards.webp",
-		"/img/portfolio2/desktop.webp",
-		"/img/portfolio2/mobile.webp",
-		"/img/portfolio2/menu.webp"
+		"img/portfolio2/cards.webp",
+		"img/portfolio2/desktop.webp",
+		"img/portfolio2/mobile.webp",
+		"img/portfolio2/menu.webp"
 	],
 	"imagePosition": "top",
 	"order": "3"
@@ -38,7 +38,7 @@ let posts = [{
 	"title": "Portfolio v.1",
 	"body": "Första försöket! Inte så mycket att säga här förutom att jag kom ännu kortare än vid andra försöket.",
 	"images": [
-		"/img/portfolio1/preview.webp"
+		"img/portfolio1/preview.webp"
 	],
 	"imagePosition": "top",
 	"order": "4"
@@ -46,11 +46,11 @@ let posts = [{
 	"title": "Västerbottensost -  varumärkeshem",
 	"body": "Slutprojekt i kursen design för användarupplevelse. Vi fick 4 veckor på oss att arbeta fram ett så kallat varumärkeshem till Västerbottensost.",
 	"images": [
-		"/img/vbost/ostprovning.webp",
-		"/img/vbost/overview.webp",
-		"/img/vbost/desktop.webp",
-		"/img/vbost/desktop2.webp",
-		"/img/vbost/mobile.webp"
+		"img/vbost/ostprovning.webp",
+		"img/vbost/overview.webp",
+		"img/vbost/desktop.webp",
+		"img/vbost/desktop2.webp",
+		"img/vbost/mobile.webp"
 	],
 	"imagePosition": "right",
 	"order": "5"
