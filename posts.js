9b561a8c9e2336e3131f1b3b7b4675c979ae7430
ChristofferBillman <christoffer.billman@gmail.com>
2022-04-17 14:45:56 +0200

    Added Lab Plant project.

--- posts.js
+++ posts.js
@@ -1,4 +1,15 @@
 let posts = [{
+	"title": "Lab Plant",
+	"body": "Detta var slutprojektet i kursen Design och Tjänsteutveckling för IoT vid Umeå Universitet. Kursen handlade till stor del om att bygga egna system och experimentera med mikrokontrollers och sensorer. I projektet byggde jag tillsammans med tre andra ett system för att övervaka och vattna krukväxter med hjälp av en ESP-32, ett urval sensorer och en Raspberry Pi 4.",
+	"images": [
+		"img/lab-plant/setup.jpeg",
+		"img/lab-plant/readme_img.png",
+		"img/lab-plant/soilsensor.jpeg",
+	],
+	"imagePosition": "right",
+	"order": "1",
+	"ghlink": "https://github.com/ChristofferBillman/ProjektIoT"
+},{
 	"title": "Portfolio v.3",
 	"body": "Den tredje försöket till en portfoliosida! Denna version kom faktiskt till, och projektet blev mycket mer ambitiöst än vad de tidigare två var! Två flugor en smäll!",
 	"images": [
@@ -7,7 +18,8 @@ let posts = [{
 		"img/portfolio3/preview.webp"
 	],
 	"imagePosition": "right",
-	"order": "1"
+	"order": "2",
+	"ghlink": "https://github.com/ChristofferBillman/Portfolio3-Deployed"
 },{
 	"title": "Skissteknik - Rumbokningsystem",
 	"body": "Denna uppgift var del av kursen 'Design för användarupplevelse' hos designhögskolan i Umeå. Det gick huvudsakligen ut på att lära sig hur man arbetar med wireframes och iterativa designprocesser",
@@ -22,7 +34,7 @@ let posts = [{
 		"img/skissteknik/m8.webp"
 	],
 	"imagePosition": "top",
-	"order": "2"
+	"order": "3"
 },{
 	"title": "Portfolio v.2",
 	"body": "Andra försöket till en portfoliosida - gick sådär. Blev aldrig riktigt nöjd med designen så det projektet kom som längst med Hi-Fi prototyper i Adobe XD.",
@@ -33,7 +45,7 @@ let posts = [{
 		"img/portfolio2/menu.webp"
 	],
 	"imagePosition": "top",
-	"order": "3"
+	"order": "4"
 },{
 	"title": "Portfolio v.1",
 	"body": "Första försöket! Inte så mycket att säga här förutom att jag kom ännu kortare än vid andra försöket.",
@@ -41,7 +53,7 @@ let posts = [{
 		"img/portfolio1/preview.webp"
 	],
 	"imagePosition": "top",
-	"order": "4"
+	"order": "5"
 },{
 	"title": "Västerbottensost -  varumärkeshem",
 	"body": "Slutprojekt i kursen design för användarupplevelse. Vi fick 4 veckor på oss att arbeta fram ett så kallat varumärkeshem till Västerbottensost.",
@@ -53,5 +65,5 @@ let posts = [{
 		"img/vbost/mobile.webp"
 	],
 	"imagePosition": "right",
-	"order": "5"
+	"order": "6"
 }]
